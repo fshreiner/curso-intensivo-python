f9fba02e0521357a3baf09f629b24e484fb05c72
Fabio Shreiner <fshreiner21@gmail.com>
2021-12-21 22:01:52 -0300

    ex01

---
+++ exercicios/name.py
@@ -0,0 +1,7 @@
+# Exemplos de uso de métodos declarados após a variável, estes úteis quando um título deve ser exibido, ou um dado deve ser colhido todo com letras maiúsculas ou minúsculas.
+
+name = "ada lovelace"
+
+print(name.title())
+print(name.upper())
+print(name.lower())
