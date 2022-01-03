a07346105232929c906237d1cccba4aff7494c98
Fabio Shreiner <fshreiner21@gmail.com>
2022-01-03 17:27:11 -0300

    for em dicionários

--- 6_Dicionarios/notas/favorite_languages.py
+++ 6_Dicionarios/notas/favorite_languages.py
@@ -6,3 +6,6 @@ favorite_languages = {
 }
 
 print("Sarah's favorite language is {}".format(favorite_languages['sarah'].title()) + '.\n')
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " + language.title() + ".")
---
+++ 6_Dicionarios/notas/user.py
@@ -0,0 +1,10 @@
+# Percorrendo todos os pares chave-valor com um laço
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last:': 'fermi',
+}
+
+for key, value in user_0.items():
+    print('\nKey: {}'.format(key))
+    print('Value: {}'.format(value))
