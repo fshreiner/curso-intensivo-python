e6dce0ebefcf7cd1f530db7dd2cd3eb4b3ae9b92
Fabio H. Shreiner <fshreiner21@gmail.com>
2022-02-02 22:15:35 -0300

    arquivos

--- 10_Arquivos_e_excecoes/notas/file_reader.py
+++ 10_Arquivos_e_excecoes/notas/file_reader.py
@@ -1,4 +1,11 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents)
+file_path = r'C:\Users\PC\Documents\GitHub\curso-intensivo-python2\curso-intensivo-python\10_Arquivos_e_excecoes\notas\pi_digits.txt'
+
+with open(file_path) as file_object:
+    for line in file_object:
+        print(line)
+
+    # contents = file_object.read()
+    # print(contents)
+
+    # pag 234
     
